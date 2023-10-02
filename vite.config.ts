8c7ac6fdedf7269258d0b106d8719bdb8ab9d39e
x47 <72315013+x47base@users.noreply.github.com>
2023-10-02 19:07:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zli-modul-294"
+  base: "/x47base.github.io"
 })
